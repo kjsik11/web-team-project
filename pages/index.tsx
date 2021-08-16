8bd8499a38c1647f7302ef4fa0c0fc6b8f9cf54a
jongsik <kjsik11@gmail.com>
2021-08-16 18:01:21 +0900

    hide iframe

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,16 +18,16 @@ const DashboardPage = () => {
       <div className="pb-6 border-b border-gray-200">
         <h3 className="text-4xl font-medium text-gray-900">My Web Project</h3>
       </div>
-
       <div className="relative my-16 h-full md:mt-8 w-full rounded-md overflow-hidden">
-        <iframe
+        숨겨진 iframe 입니다.
+        {/* <iframe
           className="w-4/5 h-4/5 absolute inset-0 mx-auto"
           frameBorder="0"
           allow="encrypted-media"
           allowFullScreen
           title="youtube"
           src="https://www.youtube.com/embed/2KCURM6Mwsw"
-        />
+        /> */}
       </div>
     </div>
   );
